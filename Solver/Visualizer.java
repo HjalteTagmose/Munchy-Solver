2779900d3234dd5218b4db4430d970deb620f34c
hjta <hjta@itu.dk>
2020-09-27 13:14:50 +0200

    nothing

--- Solver/Visualizer.java
+++ Solver/Visualizer.java
@@ -1,10 +1,8 @@
 package Solver;
 
 import javax.swing.*;
-import javax.swing.event.MouseInputListener;
 
 import java.awt.*;
-import java.awt.event.*;
 import java.util.*;
 import java.util.List;
 
